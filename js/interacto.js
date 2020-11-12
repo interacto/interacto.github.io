75a770604d009d1c7a7cfdc52d970b120bab0190
arnobl <1052622+arnobl@users.noreply.github.com>
2020-11-12 22:17:41 +0100

    search field

--- js/interacto.js
+++ js/interacto.js
@@ -1,3 +1,19 @@
+function filter() {
+  var a, i, txtValue;
+  
+  const filter = document.getElementById('filterInput').value.toUpperCase();
+  const ul = document.getElementById("content");
+  const li = ul.getElementsByClassName('item');
+
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].classList.add("show");
+    } else {
+      li[i].classList.remove("show");
+    }
+  }
+}
 
 function getSelectedLge() {
     return findActiveIn("lge");
